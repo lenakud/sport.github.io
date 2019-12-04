df5e16af85565737b2c739de706812e56dfc019d
lenakud <lenakudl31@gmail.com>
2019-12-04 14:07:53 +0300

    new section and responsive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
      e.preventDefault();
      $('.block-nav').toggleClass('is-open')
    })
+
+
+       $(function() {
+
+           $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+               $(this)
+                   .addClass('active').siblings().removeClass('active')
+                   .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+           });
+
+       });
  })
